4c7d631ebf87344bdb74c478ec56836c82f76a69
Hyun Beomseok <hbs530@naver.com>
2023-11-08 11:56:24 +0900

    6단계

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -67,6 +67,17 @@ public class App {
                 actionList();
                 // actionList() 메서드 호출
             }
+            else if (cmd.startsWith("삭제?")){
+            // 특정 문자열로 시작되는지 확인하는 startsWith 메서드를 사용하여 입력된 cmd 변수가 삭제?로 시작하는 다른 조건이면
+                
+                actionRemove(cmd);
+                // cmd를 인수로 하는 actionRemove 메서드 호출
+            }
+            else if (cmd.startsWith("수정?"))
+            // 특정 문자열로 시작되는지 확인하는 startsWith 메서드를 사용하여 입력된 cmd 변수가 수정?로 시작하는 다른 조건이면
+
+                actionModify(cmd);
+                // cmd를 인수로 하는 actionModify 메서드 호출
         }
     }
 
@@ -127,4 +138,95 @@ public class App {
             // 참조 변수 quotation에 명언을 저장하는 정수형 필드 id,작가 이름을 저장하는 문자형 필드 content,작가 이름을 저장하는 authorName 필드에 각각 접근하여 정수 출력 형식 %d, 문자열 출력 형식 %s에 각각 대입하여 출력
         }
     }
+
+    void actionRemove(String cmd){
+    // 문자열 변수 cmd를 매개변수로 하는 리턴 값이 없는 void 형 actionRemove 메서드 선언
+
+        int id = getParamAsInt(cmd, "id", 0);
+        // cmd, paranName은 "id", defaultValue를 0으로 하여 getParamAsInt 메서드를 호출한 후 리턴 값을 정수형 변수 id에 대입
+
+        if (id == 0){
+        // id가 0이면
+
+            System.out.println("id를 정확히 입력해주세요.");
+            // "id를 정확히 입력해주세요." 출력
+
+            return;
+            // if 조건문을 종료하고 호출한 getParamAsInt 메서드로 이동
+        }
+        System.out.printf("%d번 명언을 삭제합니다.\n", id);
+        // id가 0이 아니면 0이 아닌 id를 정수 출력 형식인 %d에 대입한 "%d번 명언을 삭제합니다." 출력 후 줄 바꿈
+    }
+
+    void actionModify(String cmd){
+    // 문자열 변수 cmd를 매개변수로 하는 리턴 값이 없는 void 형 actionModify 메서드 선언
+
+        int id = getParamAsInt(cmd, "id", 0);
+        // cmd, paranName은 "id", defaultValue를 0으로 하여 getParamAsInt 메서드를 호출한 후 리턴 값을 정수형 변수 id에 대입
+
+        if (id == 0){
+        // id가 0이면
+
+            System.out.println("id를 정확히 입력해주세요.");
+            // "id를 정확히 입력해주세요." 출력
+
+            return;
+            // actionModify() 메서드를 종료하고 호출한 곳으로 이동
+        }
+
+        System.out.printf("%d번 명언을 수정합니다.\n", id);
+        // id가 0이 아니면 0이 아닌 id를 정수 출력 형식인 %d에 대입한 "%d번 명언을 수정합니다." 출력 후 줄 바꿈
+    }
+    int getParamAsInt(String cmd, String paramName, int defaultValue){
+    // 문자열 변수 cmd, paramName, 정수형 변수 defaultValue를 매개변수로 하는 getParamAsInt 매서드 선언
+
+        String[] cmdBits = cmd.split("\\?", 2);
+        // 문자열 변수 cmd를 구분자를 기준으로 문자열을 잘라 배열로 입력할 떄 사용하는 split() 메서드를 이용하여
+        // 정규표현식 regex인 \\?를 기준으로 limit 크기 2만큼 잘려진 수들을 문자열 배열 변수 cmdBits에 대입
+
+        String queryString = cmdBits[1];
+        // cmdBits[1]을 문자열 변수 queryString에 대입
+
+        String[] queryStringBits = queryString.split("&");
+        // 문자열 변수 queryString을 구분자를 기준으로 문자열을 잘라 배열로 입력할 떄 사용하는 split() 메서드를 이용하여
+        // 정규표현식 regex인 & 기준으로 잘려진 수들을 문자열 배열 변수 queryStringBits에 대입
+
+        for (int i = 0; i < queryStringBits.length; i++){
+        // 0으로 초기화된 정수형 변수 i가 문자열 배열 queryStringBits 변수의 길이를 나타내는 length 속성보다 작으면 아래 코드를 실행하고 i값을 +1
+
+            String queryParamStr = queryStringBits[i];
+            // 배열 크기가 i인 문자열 배열 queryStringBits 변수를 문자열 변수 queryParamStr에 대입
+
+            String[] queryParamStrBits = queryParamStr.split("=", 2);
+            // 문자열 변수 queryParamStr를 구분자를 기준으로 문자열을 잘라 배열로 입력할 떄 사용하는 split() 메서드를 이용하여
+            // 정규표현식 regex인 = 기준으로 limit 크기 2만큼 잘려진 수들을 문자열 배열 변수 queryParamStrBits에 대입
+
+            String _paramName = queryParamStrBits[0];
+            // 배열 크기가 0인 문자열 배열 queryParamStrBits를 문자열 변수__paramName에 대입
+
+            String paramValue = queryParamStrBits[1];
+            // 배열 크기가 1인 문자열 배열 queryParamStrBits를 문자열 변수 paramValue에 대입
+
+            if(_paramName.equals(paramName)){
+            // 두 객체가 내용이 같은지 비교할 떄 사용되는 equals 메서드를 사용하여 입력된 cmd 변수가 종료와 같으면
+
+                try{
+                // 예외 발생시 처리
+
+                    return Integer.parseInt(paramValue);
+                    // 문자열 변수 paramValue를 인자로 받아 파싱하여 정수로 반환하는 Integer.parseInt() 메서드를 사용하여 리턴
+
+                }
+                catch (NumberFormatException e) {
+                // 문자열을 숫자 또는 실수로 변환할 떄 문자열이 변환하고자 하는 숫자 형식이 아니면 반환이 실패 할때 발생하는 실행 예외인 NumberFormatException 발생시 처리
+
+                    return defaultValue;
+                    // 정수형 변수 defaultValue 리턴
+                }
+            }
+        }
+        return defaultValue;
+        // 정수형 변수 defaultValue 리턴
+    }
 }
+
